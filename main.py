07735fc19110ae1ea76e4f9b906b86b96836df72
hanonymus90 <52471384+hanonymus90@users.noreply.github.com>
2020-03-12 10:20:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import time
 import random
-count= 0
-n = random.randint(1, 100)
-guess = int(input("Enter an integer from 1 to 100: "))
+
+count= 0    ##count the number of times you have guessed
+
+n = random.randint(1, 100)         ##random number too guess
+
+guess = int(input("Enter an integer from 1 to 100: "))      ##you're guess
+
 while n != "guess":
     print
     if guess < n:
         print ("you're guess is too low")
         guess = int(input("Enter an integer from 1 to 100: "))
-        count= count+ 1
+        count = count + 1
     elif guess > n:
         print ("guess is high")
         guess = int(input("Enter an integer from 1 to 100: "))
@@ -17,13 +21,18 @@ while n != "guess":
         print ("Correct!")
         print("\nProcesing all you're data, wait a little bit.")
         print("\n <----------------------------------------->")
-        time.sleep(2)
-        print("\nYou guessed " +str(count)+ " times")
-        if count<= 3:
-            print("You are a legend")
+        time.sleep(1.5)
+        print("\nYou guessed " +str(count)+ " times/time")
+        
+        if count == 1:
+            print("Omg you're a master mind subscribe and comment in my latest video and then you will get awarded if you id it without cheating (Coded armor)")
+        if count == 2 or count == 3:
+            print("You are a freaking legend")
         if count == 4 or count == 5:
             print ("you're pretty good")
-        if count >=6:
+        if count == 6 :
             print("you're good")
+        if count >= 7:
+            print("you're super bad")
         break
     print
